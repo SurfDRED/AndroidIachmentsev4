be5a76522198a2a07dc7e2dd26e54a53a6cfcb10
Dmitrii Iachmentcev <d.yachmentsev@gmail.com>
2020-02-05 00:16:06 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/androidiachmentsev4/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.androidiachmentsev4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    public void onClickButton() {
+    }
+
+    @Test
+    public void onClickBack() {
+    }
+}
---
+++ app/src/main/java/com/example/androidiachmentsev4/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.androidiachmentsev4;
+import java.util.Random;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        onClickBack();
+        onClickButton();
+    }
+
+    public void onClickButton (){
+
+        // Генерация случайных чисел с помощью класса Random
+        final Random random = new Random();
+        // найдем элементы
+        final TextView exampleText = findViewById(R.id.textView2);
+        Button BtnForward = findViewById(R.id.butForward);
+        // создаем обработчик нажатия Forward и выводим информацию
+        exampleText.setText("http://myfile.org/"+ String.valueOf(random.nextInt(100)));
+
+        BtnForward.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+
+
+    public void onClickBack (){
+        Button BtnBack = findViewById(R.id.butBack);
+        // создаем обработчик нажатия Back
+        BtnBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
